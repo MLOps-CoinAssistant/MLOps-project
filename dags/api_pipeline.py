f821a46f96fca581cb2af1e4a9e0455f012c363a
dohyung <dohk325@gmail.com>
2024-05-15 07:48:14 +0000

    [fix] #6: revise dependencies

--- dags/api_pipeline.py
+++ dags/api_pipeline.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # Airflow DAG를 정의합니다.
 @dag(
     schedule_interval="0 * * * *",  # 매 시간 0분에 실행
-    start_date=datetime(2023, 5, 15),
+    start_date=datetime(2024, 5, 15),
     catchup=False,  # 이전 실행은 무시합니다.
     default_args={"owner": "Astro", "retries": 3},
     tags=["upbit_to_cloudsql"],
