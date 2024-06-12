10860cfc17fd6a7c6de1d1eb9e7e6b7f248e3eab
Kwon DoHyung <dohk325@gmail.com>
2024-06-12 09:07:58 +0000

    fix: Add more commands related to conda and poetry for dependencies

--- backend-start.sh
+++ backend-start.sh
@@ -3,11 +3,20 @@
 # Conda 초기화
 eval "$(conda shell.bash hook)"
 
-# Conda 환경 활성화
-conda activate fastapi
+# Conda 환경이 있는지 확인하고, 없으면 생성
+if conda env list | grep -q "fastapi"; then
+    conda activate fastapi
+else
+    conda create -n fastapi python=3.11 -y
+    conda activate fastapi
+fi
 
 # 작업 디렉토리 변경
 cd fastapi
+
+# Poetry 설치 및 종속성 설치
+pip install poetry
+poetry install
 
 # 환경 변수 설정
 export AWS_ACCESS_KEY_ID=None
