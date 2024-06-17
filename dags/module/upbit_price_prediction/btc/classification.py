fix: remove od_type, od_wait & add logic to control f1_score's average parameter along with data-imbalanced

dags/module/upbit_price_prediction/btc/classification.py
@@ -15,6 +15,8 @@ import pandas as pd
 import mlflow
 import uvloop
 import time
+import numpy as np
+
 
 # uvloop를 기본 이벤트 루프로 설정
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
@@ -75,14 +77,23 @@ def train_catboost_model_fn(**context: dict) -> None:
             "border_count": trial.suggest_int("border_count", 32, 255),  # 분할 수 설정
             "feature_border_type": trial.suggest_categorical("feature_border_type", ["Median", "Uniform", "UniformAndQuantiles", "MaxLogSum", "MinEntropy", "GreedyLogSum"]),  # 경계 유형 설정
             "random_strength": trial.suggest_float("random_strength", 1e-3, 10, log=True),  # 랜덤화 강도 조절
-            "od_type": "Test", # 검증 데이터에 대하여 성능 개선이 이루어지지 않을 경우 조기 종료
-            "od_wait": 10, # 10번의 학습에서 성능 개선이 이루어지지 않을 경우 조기 종료
         }
 
         model = CatBoostClassifier(**params, logging_level="Info")
         model.fit(train_pool, eval_set=valid_pool, early_stopping_rounds=50)
         preds = model.predict(valid_pool)
-        return f1_score(y_valid, preds, average="micro")
+        
+        # 클래스 분포 확인
+        class_distribution = np.bincount(y_valid)
+        imbalance_ratio = class_distribution.max() / class_distribution.min()
+
+        # 불균형 정도에 따라 macro, micro 설정
+        if imbalance_ratio > 1.5:  # 임계값은 데이터셋에 따라 조정
+            average = "macro"
+        else:
+            average = "micro"
+
+        return f1_score(y_valid, preds, average=average)
 
     # Optuna 설정
     postgresHook: PostgresHook = PostgresHook(